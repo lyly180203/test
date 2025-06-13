b41f3225d1a54041ac61f18b055156fe40536ab1
lyly <lyly@gmail.com>
2025-06-13 19:47:32 +0700

    Cập nhật giao diện hiển thị thông tin sinh viên

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,8 @@ function App() {
 
   return (
     <>
-     <h1 className='hello-container'>Test deloy</h1>
+     <h1 className='hello-container'>Thông tin sinh viên</h1>
+     <h2>Lê Thị Ly Ly -DH52104298</h2>
     </>
   )
 }
